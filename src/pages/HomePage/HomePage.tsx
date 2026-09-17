8001c6da3de17c5417641af4de26f84814df29ce
grekovpavel019 <grekovpavel065@gmail.com>
2026-09-18 00:58:15 +0300

    feature: create new header

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -4,61 +4,23 @@ import styles from "./HomePage.module.scss";
 
 const HomePage: FC = (): React.JSX.Element => {
     return (
-        <div className={styles.page}>
-            <header className={styles.header}>
-                <div className="container">
-                    <div className={styles.headerContent}>
-                        <h1 className={styles.logo}>
-                            Новая Жизнь
-                        </h1>
-
-                        <nav className={styles.navigation}>
-                            <ul>
-                                <li><a href="">О клинике</a></li>
-                                <li><a href="">Услуги</a></li>
-                                <li><a href="">Цены</a></li>
-                            </ul>
-
-                            <button className="primaryButton">
-                                Запись
-                            </button>
-                        </nav>
-
-                    </div>
+        <header className={styles.siteHeader}>
+            <div className={`container ${styles.headerInner}`}>
+                <div className={styles.logo}>
+                    Капельник
                 </div>
-            </header>
 
-            <main className={styles.main}>
+                <nav className={styles.mainNav}>
+                    <a className={styles.navLink} href="">Главная</a>
+                    <a className={styles.navLink} href="">О клинике</a>
+                    <a className={styles.navLink} href="">Услуги</a>
+                </nav>
 
-                <section className={styles.hero}>
-                    <div className="container">
-
-                        <div className={styles.heroContent}>
-                            <h1 className={styles.heroTitle}>Помощь начинается с первого шага</h1>
-                            <p className={styles.heroDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui nihil quam iure accusantium sapiente nostrum facilis in praesentium!</p>
-                            <div className={styles.buttonArea}>
-
-                                <button className="primaryButton">Получить консультацию</button>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-
-                <section className={styles.advantage}>
-                    <div className="container">
-
-                        <div className={styles.advantageContent}>
-
-                        </div>
-
-                    </div>
-                </section>
-            </main>
-
-            <footer className={styles.footer}>
-                Подвал
-            </footer>
-        </div>
+                <div className="actions">
+                    <button className={styles.buttonPrimary}>Получить консультацию</button>
+                </div>
+            </div>
+        </header>
     );
 };
 
