6094a8b66d239740ae80cbf6c8e4fffef06e37b5
grekovpavel019 <grekovpavel065@gmail.com>
2026-09-18 16:25:44 +0300

    feature: add hero section

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,43 +1,14 @@
 import React, { type FC } from "react";
 
 import Header from "@/sections/Header";
-
-import styles from "./HomePage.module.scss";
+import Hero from "@/sections/Hero";
 
 const HomePage: FC = (): React.JSX.Element => {
     return (
         <>
             <Header />
-
             <main>
-                <section className={`${styles.hero} container`}>
-                    <div className={styles.heroIntro}>
-                        <span className={styles.eyebrow}>
-                            + Частная Клиника "Новый шаг"
-                        </span>
-
-                        <h1 className={styles.heroHeader}>
-                            Помощь в борьбе с алкогольной зависимостью
-                        </h1>
-
-                        <p className={styles.heroDescription}>
-                            Консультация специалистов, лечение алкогольной зависимости и программы кодирования с индивидуальным подходом к каждому пациенту.
-                        </p>
-
-                        <div className={styles.heroActions}>
-                            <button className="buttonPrimary">Получить консультацию</button>
-                            <button className="buttonSecondary">Наши услуги</button>
-                        </div>
-
-                        <div className={styles.heroNote}>
-                            Мы не даём медицинских гарантий и не обещаем конкретный результат — подход подбирается индивидуально после консультации со специалистом.
-                        </div>
-                    </div>
-
-                    <div className={styles.card}>
-                        123
-                    </div>
-                </section>
+                <Hero />
             </main>
 
         </>
---
+++ src/sections/Hero/Hero.tsx
@@ -0,0 +1,40 @@
+import React, { type FC } from "react";
+
+import LinkButton from "@/shared/components/LinkButton";
+
+import styles from "./Hero.module.scss";
+
+const Hero: FC = (): React.JSX.Element => {
+    return (
+        <section className={`${styles.hero} container`}>
+            <div className={styles.heroIntro}>
+                <span className={styles.eyebrow}>
+                    + Частная Клиника "Новый шаг"
+                </span>
+
+                <h1>
+                    Помощь в борьбе с алкогольной зависимостью
+                </h1>
+
+                <p>
+                    Консультация специалистов, лечение алкогольной зависимости и программы кодирования с индивидуальным подходом к каждому пациенту.
+                </p>
+
+                <div className={styles.actions}>
+                    <LinkButton variant="primaryButton">Получить консультацию</LinkButton>
+                    <LinkButton variant="secondaryButton">Наши услуги</LinkButton>
+                </div>
+
+                <div className={styles.note}>
+                    Мы не даём медицинских гарантий и не обещаем конкретный результат — подход подбирается индивидуально после консультации со специалистом.
+                </div>
+            </div>
+
+            <div className={styles.card}>
+                123
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
